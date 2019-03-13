32f24e6f98ad8d26c8f2f17d3e5a641995e5f210
GuillaumeDua <guillaume.dua@gmail.com>
2019-03-14 00:27:36 +0100

    original POC : countdown OK

--- CPDJ_VirtualLock/MainWindow.xaml.cs
+++ CPDJ_VirtualLock/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CPDJ_VirtualLock
 {
@@ -20,9 +21,39 @@ namespace CPDJ_VirtualLock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private TimeSpan remaining_time;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            StartTimer(TimeSpan.FromSeconds(10));
+        }
+
+        private void StartTimer(TimeSpan time)
+        {
+            remaining_time = time;
+            timer = new DispatcherTimer
+            (
+                TimeSpan.FromSeconds(1),
+                DispatcherPriority.Normal,
+                delegate
+                {
+                    ui_countdown.Text = remaining_time.ToString("c");
+                    if (remaining_time == TimeSpan.Zero)
+                    {
+                        timer.Stop();
+                        OnTimerStop();
+                    }
+                    remaining_time = remaining_time.Subtract(TimeSpan.FromSeconds(1));
+                }, Application.Current.Dispatcher
+            );
+        }
+
+        private void OnTimerStop()
+        {
+
         }
     }
 }
