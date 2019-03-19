b093ad96d0bae5ccc38bd9d5f58aa5a12265c559
GuillaumeDua <guillaume.dua@gmail.com>
2019-03-19 01:01:27 +0100

    add OnPlayerBadInput : remaining_try

--- CPDJ_VirtualLock/MainWindow.xaml.cs
+++ CPDJ_VirtualLock/MainWindow.xaml.cs
@@ -23,14 +23,15 @@ namespace CPDJ_VirtualLock
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private DispatcherTimer timer;
-        private TimeSpan remaining_time = TimeSpan.Zero;
-
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
         }
+
+        #region Countdown
+        private DispatcherTimer timer;
+        private TimeSpan remaining_time = TimeSpan.Zero;
 
         private void StartTimer(TimeSpan time)
         {
@@ -53,21 +54,70 @@ namespace CPDJ_VirtualLock
                 }, Application.Current.Dispatcher
             );
         }
+        #endregion
 
+        #region GamePlay
+        private void OnPlayerInput(String input_value)
+        {
+            if (input_value == "toto") // todo : as configuration
+                OnPlayerSuccess();
+            else
+                OnPlayerBadInput();
+        }
+        public String remaining_try
+        {
+            get { return _remaining_try.ToString(); }
+        }
+        private int _remaining_try = 3;
+        private void OnPlayerBadInput()
+        {
+            // add to try list
+
+            _remaining_try -= 1;
+            OnPropertyChanged("remaining_try");
+            if (_remaining_try == 0)
+            {
+                // freeze
+                _remaining_try = 3;
+            }
+        }
         private void OnPlayerDefeat()
         {
+            timer.Stop(); // useless
             ui_grid_countdown.Visibility = Visibility.Collapsed;
             ui_grid_failure.Visibility = Visibility.Visible;
         }
+        private void OnPlayerSuccess()
+        {
+            timer.Stop();
+            ui_grid_countdown.Visibility = Visibility.Collapsed;
+            ui_grid_success.Visibility = Visibility.Visible;
+        }
+        #endregion
 
         private void ui_start_button_clicked(object sender, RoutedEventArgs e)
         {
             var start_button = sender as Button;
             if (start_button == null)
                 throw new ArgumentException("ui_start_button_clicked:sender");
+
             start_button.Visibility = Visibility.Hidden;
+            ui_dock_password.Visibility = Visibility.Visible;
+            ui_passwordBox.Focus();
 
             StartTimer(TimeSpan.FromSeconds(10));
+        }
+        private void ui_passwordBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            var password_box = sender as PasswordBox;
+
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                e.Handled = true;
+                String value = password_box.Password;
+                password_box.Clear();
+                OnPlayerInput(value);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
