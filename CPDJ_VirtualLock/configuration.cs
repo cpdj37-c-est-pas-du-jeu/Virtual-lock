c5720f65f210d30c3f96afdb7dd303d096f070ac
GuillaumeDua <guillaume.dua@gmail.com>
2019-03-22 00:57:13 +0100

    cosmetics changes. issue with player's input field font scaling

---
+++ CPDJ_VirtualLock/configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPDJ_VirtualLock
+{
+    struct configuration
+    {
+        public TimeSpan total_duration;
+
+        public String password;
+
+        #region player attempts
+        public uint try_before_lock; // what if 0 ?
+        public bool lock_is_final;
+        #endregion
+    }
+}
